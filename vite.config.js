34250b69f0c1796c2b99e33ecaae9e7a4da46db5
Baat01 <baat01.p@gmail.com>
2026-06-22 23:54:35 -0400

    Configure GitHub Pages automated deployment

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  server: {
-    port: 5173,
-  },
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'serve' ? '/' : '/SmashThumbnailGenerator/',
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    server: {
+      port: 5173,
+    },
+  }
 })
+
